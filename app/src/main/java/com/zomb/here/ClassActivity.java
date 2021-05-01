2a0e4fdf276d5c6c0942eb0c2c54ac39e68e5527
Zombie-Muse <gjmccormack@cpp.edu>
2021-05-01 08:46:18 -0700

    Initial commit

---
+++ app/src/main/java/com/zomb/here/ClassActivity.java
@@ -0,0 +1,13 @@
+package com.zomb.here;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClassActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class);
+    }
+}
---
+++ app/src/main/java/com/zomb/here/MainActivity.java
@@ -0,0 +1,69 @@
+package com.zomb.here;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String user = "Christy";
+    TextView greeting;
+    AppCompatButton classes;
+    AppCompatButton students;
+    AppCompatButton reports;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        greeting = findViewById(R.id.tv_greeting);
+        greeting.setText("Greetings, " + user + "!");
+
+        classes = findViewById(R.id.btn_classes);
+        classes.setOnClickListener(v -> showClasses());
+
+        students = findViewById(R.id.btn_students);
+        students.setOnClickListener(v -> showStudents());
+
+        reports = findViewById(R.id.btn_reports);
+        reports.setOnClickListener(v -> showReports());
+        setToolbar();
+        setSupportActionBar(toolbar);
+
+    }
+
+    private void setToolbar() {
+        toolbar = findViewById(R.id.toolbar_main);
+        TextView title = toolbar.findViewById(R.id.toolbar_title);
+        ImageButton back = toolbar.findViewById(R.id.btn_back);
+        ImageButton save = toolbar.findViewById(R.id.btn_save);
+
+        title.setText("Here!");
+        back.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+    }
+
+    private void showClasses() {
+        Intent intent = new Intent(this, ClassActivity.class);
+        startActivity(intent);
+    }
+
+    private void showStudents() {
+//        Intent intent = new Intent(this, StudentActivity.class);
+//        startActivity(intent);
+    }
+
+    private void showReports() {
+//        Intent intent = new Intent(this, ReportActivity.class);
+//        startActivity(intent);
+
+    }
+}
